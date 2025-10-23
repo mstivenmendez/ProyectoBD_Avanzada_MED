a4bb32b6857068b8567ce2f795a8f230f529ee21
mstivenmendez <maicollstivendm3@hotmail.com>
2025-10-23 17:33:47 -0500

    feat: :sparkles: creacion de insersionn de datos de la tabla de categoria

---
+++ 01_Esquema_y_Datos/e_commerce_dml.sql
@@ -0,0 +1,10 @@
+--- insercion de categorias
+INSERT INTO categoria (descripcion, nombre, iva)
+VALUES ('Calzado deportivo y casual para hombres, mujeres y niños', 'Calzado', 19.00);
+INSERT INTO categoria (descripcion, nombre, iva)
+VALUES ('Prendas de vestir como camisetas, pantalones y chaquetas', 'Ropa', 19.00);
+INSERT INTO categoria (descripcion, nombre, iva)
+VALUES ('Dispositivos electrónicos como celulares, tablets y accesorios', 'Electronico', NULL);
+INSERT INTO categoria (descripcion, nombre, iva)
+VALUES ('Artículos para el hogar, cocina y decoración', 'Hogar', 5.00);
+
